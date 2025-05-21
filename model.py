ad653fae8f60da51e874bb39d7b2f000207a8151
BriboSD <Gschwinden23@gmail.com>
2025-05-20 21:13:06 -0700

    Completed basics, still some issues. Switched to colab for now
    Completed the basic elements of the LoRA stuff. It will not run, however, due to some performance stuff (running out of RAM) and other yet to be identified issues. Should figure out Colab, switch model perhaps to one thats less taxing, and continue

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments 
 import pandas as pd
+from datasets import Dataset
+from peft import get_peft_model, LoraConfig, PeftModel
 from sklearn.model_selection import train_test_split
+import os
 import torch
 
 #read in info from info.csv
-df = pd.read("data/info.csv")
+df = pd.read_csv("data/info.csv")
 
 #split data into training and validation.
 #test_size = 0.1 --> 10% is validation
@@ -19,12 +22,56 @@ model_id = "google/gemma-3-1b-it"  # or "google/gemma-1.1-7b-it" if you have a s
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id)
 
+lora_config = LoraConfig(
+    r=4, #As bigger the R bigger the parameters to train.
+    lora_alpha=2, # a scaling factor that adjusts the magnitude of the weight matrix. Usually set to 1
+    target_modules=["q_proj", "v_proj"], #You can obtain a list of target modules in the URL above.
+    lora_dropout=0.05, #Helps to avoid Overfitting.
+    bias="lora_only", # this specifies if the bias parameter should be trained.
+    task_type="CAUSAL_LM"
+)
+
+label2id = {"Conservative": 0, "Liberal": 1}
 def tokenize_function(examples):
-    return tokenizer(examples['Text'], padding='max_length', truncation=True)
+    tokens = tokenizer(
+        examples["Title"],
+        padding="max_length",
+        truncation=True,
+        max_length=128
+    )
+    tokens["labels"] = label2id[examples["Political Lean"]]
+
+    return tokens
 
 # Tokenize dataset splits so readable to computer
-train_dataset = train_df.map(tokenize_function, batched=True)
-val_dataset = val_df.map(tokenize_function, batched=True)
+
+train_dataset = Dataset.from_pandas(train_df)
+val_dataset = Dataset.from_pandas(val_df)
+
+train_dataset = train_dataset.map(tokenize_function, batched=True)
+val_dataset = val_dataset.map(tokenize_function, batched=True)
+
+
+peft_model = get_peft_model(model, lora_config)
+
+temp_output_dir = os.path.join('./', "peft_lab_outputs")
+
+training_args = TrainingArguments(
+    output_dir=temp_output_dir,
+    auto_find_batch_size=True, # Find a correct bvatch size that fits the size of Data.
+    learning_rate= 3e-2, # Higher learning rate than full fine-tuning.
+    num_train_epochs=2,
+    use_cpu=True
+)
+
+trainer = Trainer(
+    model=peft_model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+)
+
+trainer.train()
 
 #done for now. Next I will pair labels and political lean via a map so that the model can predict political sentiment and guess responses
 # Then I will install LoRA and implement it using peft.  
